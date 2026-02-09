52453b4d6bc0a00b6a96a0aa806d44537bc85297
RAWx18 <rawx18.dev@gmail.com>
2026-02-09 17:27:19 +0530

    Create AuthorityClient class
    Signed-off-by: RAWx18 <rawx18.dev@gmail.com>

--- caracal/sdk/__init__.py
+++ caracal/sdk/__init__.py
@@ -2,9 +2,17 @@
 Python SDK for Caracal Core.
 
 Provides developer-friendly API for budget checks and metering.
+Also provides authority enforcement SDK for mandate management.
 """
 
 from caracal.sdk.client import CaracalClient
 from caracal.sdk.context import BudgetCheckContext
+from caracal.sdk.authority_client import AuthorityClient
+from caracal.sdk.async_authority_client import AsyncAuthorityClient
 
-__all__ = ["CaracalClient", "BudgetCheckContext"]
+__all__ = [
+    "CaracalClient",
+    "BudgetCheckContext",
+    "AuthorityClient",
+    "AsyncAuthorityClient",
+]
