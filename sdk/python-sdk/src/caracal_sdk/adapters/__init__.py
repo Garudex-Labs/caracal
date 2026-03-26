79d1d47d25768a01b90ec849320f73df5b888004
RAWx18 <rawx18.dev@gmail.com>
2026-03-26 22:24:57 +0530

    feat: Add initial implementation of Caracal SDK with core components and mandate operations

---
+++ sdk/python-sdk/src/caracal_sdk/adapters/__init__.py
@@ -0,0 +1,20 @@
+"""
+Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
+Caracal, a product of Garudex Labs
+
+SDK Transport Adapters.
+"""
+
+from caracal_sdk.adapters.base import BaseAdapter, SDKRequest, SDKResponse
+from caracal_sdk.adapters.http import HttpAdapter
+from caracal_sdk.adapters.mock import MockAdapter
+from caracal_sdk.adapters.websocket import WebSocketAdapter
+
+__all__ = [
+    "BaseAdapter",
+    "SDKRequest",
+    "SDKResponse",
+    "HttpAdapter",
+    "MockAdapter",
+    "WebSocketAdapter",
+]
