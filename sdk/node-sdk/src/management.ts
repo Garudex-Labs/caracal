211e29eb95eead6d376daa6fdaf9a3126299e65b
RAWx18 <rawx18.dev@gmail.com>
2026-04-12 16:20:10 +0530

    chore: remove deprecated SDK modules and classes: delegation, ledger, management, mandates, migration, principals, and runtime_surface

--- sdk/node-sdk/src/management.ts
+++
@@ -1,6 +0,0 @@
-/** Grouped management operations for the Node SDK. */
-
-export { PrincipalOperations } from './principals';
-export { MandateOperations } from './mandates';
-export { DelegationOperations } from './delegation';
-export { LedgerOperations } from './ledger';
--- sdk/node-sdk/src/migration.ts
+++
@@ -1,4 +0,0 @@
-/** Grouped migration-oriented SDK surface. */
-
-export { CaracalClient, CaracalBuilder, SDKConfigurationError } from './client';
-export { ContextManager, ScopeContext } from './context';
