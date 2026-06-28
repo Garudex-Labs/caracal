aff3adcfaebac409ed783f0b5d188c65b01449b1
RAWx18 <rawx18.dev@gmail.com>
2026-06-28 16:59:30 +0530

    feat(db): add Operator model-provider registry migration scripts

---
+++ infra/postgres/migrations/0008_operator_ai_providers.down.sql
@@ -0,0 +1,6 @@
+-- Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
+-- Caracal, a product of Garudex Labs
+--
+-- Reverses the Operator model-provider registry table; development and CI only, never invoked by production tooling.
+
+DROP TABLE IF EXISTS public.operator_ai_providers;
