bd4d05c2c74fc74430ee911a0dbf97ac9ae0a857
RAWx18 <rawx18.dev@gmail.com>
2026-07-05 11:07:20 +0530

    feat: add ASGI middleware and FastMCP adapter documentation and implementation

---
+++ packages/adapters/fastmcp/python/caracalai_fastmcp/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
+# Caracal, a product of Garudex Labs
+#
+# caracalai_fastmcp: FastMCP adapter for Caracal-issued JWTs.
+
+from .middleware import CaracalAuth, CaracalAuthError
+
+__all__ = ["CaracalAuth", "CaracalAuthError"]
