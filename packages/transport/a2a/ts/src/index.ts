f8f4f9b311b9601765c36d7e102d31ac00ccbf80
RAWx18 <rawx18.dev@gmail.com>
2026-07-05 11:09:38 +0530

    refactor: remove obsolete A2A transport package and related documentation

--- packages/transport/a2a/ts/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2026 Garudex Labs.  All Rights Reserved.
-// Caracal, a product of Garudex Labs
-//
-// Public surface of @caracalai/transport-a2a: A2A protocol primitives.
-
-export * from './types.js'
-export * from './a2a.js'
