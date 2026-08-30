7ec19534829da7b44b94b1c9cdeef8bbc339ca68
RAWx18 <rawx18.dev@gmail.com>
2026-08-30 22:04:54 +0530

    build(opentofu): add gcp storage to user data sh

---
+++ infra/opentofu/gcp/storage.tf
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2026 Ryan Madhuwala <rawx18.dev@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+resource "google_storage_bucket" "backups" {
+  name          = "${local.name}-backups-${var.project_id}"
+  location      = var.region
+  force_destroy = var.environment != "prod"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = var.backup_nearline_days
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "NEARLINE"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = var.backup_retention_days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
