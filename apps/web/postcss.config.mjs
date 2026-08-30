e750c2cae2092d0a170fcffa5655a134d0b943d2
RAWx18 <rawx18.dev@gmail.com>
2026-08-30 22:04:31 +0530

    feat(web): add web pnpm workspace to caracal horizontal
    Co-authored-by: Gilfoyle403 <vishwa06.dev@gmail.com>

---
+++ apps/web/postcss.config.mjs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Ryan Madhuwala <rawx18.dev@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
