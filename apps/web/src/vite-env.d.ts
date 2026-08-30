bfc69a65569d66026cfd5837b24f0dcc447b8cbb
RAWx18 <rawx18.dev@gmail.com>
2026-08-30 22:04:46 +0530

    feat(web): add vite env d
    Co-authored-by: caracalCore <caracal.core@gmail.com>

---
+++ apps/web/src/vite-env.d.ts
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2026 Ryan Madhuwala <rawx18.dev@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly VITE_APP_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
