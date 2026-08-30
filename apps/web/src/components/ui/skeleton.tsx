5c43fe63b007357a00210c2f14d09a7467d9a0c1
RAWx18 <rawx18.dev@gmail.com>
2026-08-30 22:04:36 +0530

    feat(web): add ui sheet to skeleton
    Co-authored-by: avniX-dev <avni0802.dev@gmail.com>

---
+++ apps/web/src/components/ui/skeleton.tsx
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Ryan Madhuwala <rawx18.dev@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+import { cn } from "@/lib/utils"
+
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("animate-pulse rounded-md bg-primary/10", className)} {...props} />
+}
+
+export { Skeleton }
